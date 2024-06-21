dd13b86d305c9c538e6a81cc1ef93ff480378b41
haohan <han.chang@i2csolution.com>
2024-06-21 16:35:15 +0800

    add mock data in some routes and now controller hold the path property

--- CRUDroutes.js
+++ CRUDroutes.js
@@ -1,72 +1,10 @@
 import express from "express";
 
 import { toArray, createBulkConnectMiddleware } from "./model/helper.js";
-import { getController } from "./controller/controller.js";
+import { controllers } from "./controller/controller.js";
 
-const routerConfig = [
-  {
-    path: "/test",
-    controllerName: "Test",
-    middlewares: {
-      read: [
-        "Company",
-        "CompanyInvoice",
-        "ContactType",
-        "Setting",
-        "Country",
-        "City",
-        "Lang",
-        "Currencies",
-        "PaymentType",
-        "Payment",
-        "department",
-        "GetNo",
-        "CodeType",
-        "MediaType",
-        "countyDistrict",
-        "Sex",
-        "personTitle",
-        "Career",
-        "TagType",
-        "Tag",
-        "User",
-        "Permission",
-        "PermissionType",
-        "SupplierType",
-        "Supplier",
-        "SupplierContactPerson",
-        "SupplierContactType",
-        "SupplierMeta",
-        "StockBrand",
-        "StockCategory",
-        "StockCategoryMedia",
-        "StockUnit",
-        "TaxType",
-        "TaxRate",
-        "MbflagType",
-        "Stock",
-        "StockMedia",
-        "StockMeta",
-        "PriceType",
-        "StockPrice",
-        "StockSerial",
-        "StockSerialMeta",
-        "StockSupplier",
-        "StockBom",
-        "StockBomDetail",
-        "StockBomMeta",
-        "WarehouseType",
-        "Warehouse",
-        "Inventory",
-        "InventoryLog",
-      ],
-    },
-  },
-];
-
-const createRouter = ({ path, middlewares, controllerName }) => {
+const createRouter = ({ path, middlewares, actions }) => {
   const router = express.Router();
-  const controller = getController(controllerName);
 
   [
     ["create", "post"],
@@ -74,24 +12,27 @@ const createRouter = ({ path, middlewares, controllerName }) => {
     ["update", "put"],
     ["delete", "delete"],
   ].forEach(([action, method]) => {
-    if (!controller?.[action]) return;
-    try {
+    if (!actions?.[action]) return;
+
+    appendMiddleware: try {
+      if (!middlewares || !middlewares[action]) break appendMiddleware;
+
       const connectMiddleware = createBulkConnectMiddleware(
         middlewares[action]
       );
-
       router.use(connectMiddleware);
     } catch (error) {
       console.warn(error);
     }
-    router[method]("/", ...toArray(controller[action]));
+
+    router[method]("/", ...toArray(actions[action]));
   });
 
-  return { path, router };
+  return { path: `/${path}`, router };
 };
 
 export const createCRUDRoutes = (app) =>
-  routerConfig.forEach((config) => {
-    const { path, router } = createRouter(config);
+  controllers.forEach((controller) => {
+    const { path, router } = createRouter(controller);
     app.use(path, router);
   });
--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,125 @@
+const controllers = [
+  {
+    path: "stock",
+    actions: {
+      read: [
+        async (req, res) =>
+          res.response(200, "In Stock route.", {
+            total: 0,
+            totalPages: 0,
+            list: [],
+          }),
+      ],
+    },
+  },
+  {
+    path: "stock-category",
+    actions: {
+      read: [
+        async (req, res) =>
+          res.response(200, "In stock-category route.", {
+            total: 1,
+            totalPages: 1,
+            list: [
+              {
+                id: 1,
+                name: "零食",
+              },
+              {
+                id: 2,
+                name: "飲料",
+              },
+              {
+                id: 3,
+                name: "用品",
+              },
+              {
+                id: 4,
+                name: "其他",
+              },
+            ],
+          }),
+      ],
+    },
+  },
+  {
+    path: "stock-serial",
+    actions: {
+      read: [
+        async (req, res) =>
+          res.response(200, "In stock-serial route.", {
+            total: 1,
+            totalPages: 1,
+            list: [
+              {
+                id: 1,
+                name: "用品",
+              },
+              {
+                id: 2,
+                name: "雜貨",
+              },
+              {
+                id: 3,
+                name: "飲品",
+              },
+            ],
+          }),
+      ],
+    },
+  },
+  {
+    path: "supplier",
+    actions: {
+      read: [
+        async (req, res) =>
+          res.response(200, "In supplier route.", {
+            total: 1,
+            totalPages: 1,
+            list: [
+              {
+                id: 1,
+                name: "麥當勞",
+              },
+              {
+                id: 2,
+                name: "肯德基",
+              },
+              {
+                id: 3,
+                name: "星巴克",
+              },
+            ],
+          }),
+      ],
+    },
+  },
+  {
+    path: "accounting",
+    actions: {
+      read: [
+        async (req, res) =>
+          res.response(200, "In accounting route.", {
+            total: 1,
+            totalPages: 1,
+            list: [
+              {
+                id: 1,
+                name: "自己記",
+              },
+              {
+                id: 2,
+                name: "小寶記",
+              },
+              {
+                id: 3,
+                name: "豪子記",
+              },
+            ],
+          }),
+      ],
+    },
+  },
+];
 
-const controllerDict = {
-  Test: {
-    read: [async (req, res) => res.response(200, "In Test route.")]
-  }
-}
-
-export const getController = (name) => controllerDict[name];
+export { controllers };
